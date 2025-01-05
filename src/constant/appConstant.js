38027ade86d6b3e16ac2626249a5648b35aad424
Nhan Tran <138663806+NhanTran262@users.noreply.github.com>
2025-01-05 22:28:38 +0700

    Update

--- src/constant/appConstant.js
+++ src/constant/appConstant.js
@@ -1,2 +1,3 @@
-export const ELEVATOR_WEB_API = "http://localhost:8080/api"
+// export const ELEVATOR_WEB_API = "http://localhost:8080/api"
+export const ELEVATOR_WEB_API = "http://112.78.4.117:8080/api"
 export const DEFAULT_FLOOR = 1
